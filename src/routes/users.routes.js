35f71e316be19b179ae2c88fd7d313da1d62048e
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-06 02:39:44 -0600

    feat(users.routes): agregando bloques trycatch para en los metodos http

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,42 +5,62 @@ const router = express.Router();
 const service = new UsersService();
 
 router.get("/", async (req, res) => {
-	const users = await service.findAll();
-	res.json(users);
+	try {
+		const users = await service.findAll();
+		res.json(users);
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
 });
 
 router.get("/:id", async (req, res) => {
-	const { id } = req.params;
-	const user = await service.findOne(id);
-	res.json(user);
+	try {
+		const { id } = req.params;
+		const user = await service.findOne(id);
+		res.json(user);
+	} catch (error) {
+		return res.status(404).json({ message: error.message });
+	}
 });
 
 router.post("/", async (req, res) => {
-	const body = req.body;
-	const newUser = await service.create(body);
-	res.json({
-		message: "created user",
-		newUser,
-	});
+	try {
+		const body = req.body;
+		const newUser = await service.create(body);
+		res.json({
+			message: "created user",
+			newUser,
+		});
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
 });
 
 router.patch("/:id", async (req, res) => {
-	const { id } = req.params;
-	const body = req.body;
-	const userModified = await service.update(id, body);
-	res.json({
-		message: "updated",
-		userModified,
-	});
+	try {
+		const { id } = req.params;
+		const body = req.body;
+		const userModified = await service.update(id, body);
+		res.json({
+			message: "updated",
+			userModified,
+		});
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
 });
 
 router.delete("/:id", async (req, res) => {
-	const { id } = req.params;
-	const response = await service.delete(id);
-	res.json({
-		message: "deleted",
-		response,
-	});
+	try {
+		const { id } = req.params;
+		const response = await service.delete(id);
+		res.json({
+			message: "deleted",
+			response,
+		});
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
 });
 
 module.exports = router;
