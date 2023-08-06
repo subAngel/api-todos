38aef0fe6aba9ed3e1f5278a650163cd17947208
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-06 02:44:18 -0600

    feat(error.handler): creando un middleware para trackear errores

---
+++ src/middlewares/error.handler.js
@@ -0,0 +1,15 @@
+const debug = require("debug")("error-handler");
+
+function logErrors(err, req, res, next) {
+	debug(err);
+	next(err);
+}
+
+function errorHandler(err, req, res, next) {
+	res.status(500).json({
+		message: err.message,
+		stack: err.stack,
+	});
+}
+
+module.exports = { logErrors, errorHandler };
