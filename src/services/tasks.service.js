d215a11dfbb1e70a5bd452d4356d15505ee3ae39
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-06 02:10:58 -0600

    feat(services): creando la clase manejadora de los servicios para usuarios y tareas

--- src/services/tasks.service.js
+++ src/services/tasks.service.js
@@ -1,3 +1,27 @@
-class TasksService {}
+class TasksService {
+	constructor() {
+		this.tasks = [];
+	}
+
+	findAll() {
+		return this.tasks;
+	}
+
+	findOne(id) {
+		return id;
+	}
+
+	create(data) {
+		return data;
+	}
+
+	delete(id) {
+		return id;
+	}
+
+	update(id) {
+		return id;
+	}
+}
 
 module.exports = TasksService;
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,3 +1,27 @@
-class UsersService {}
+class UsersService {
+	constructor() {
+		this.users = [];
+	}
+
+	findAll() {
+		return this.users;
+	}
+
+	findOne(id) {
+		return id;
+	}
+
+	create(data) {
+		return data;
+	}
+
+	delete(id) {
+		return id;
+	}
+
+	update(id) {
+		return id;
+	}
+}
 
 module.exports = UsersService;
