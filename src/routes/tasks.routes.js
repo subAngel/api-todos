ce81993578dc7f9c9f23b1ed60253142d62b2a9d
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-06 01:58:23 -0600

    feat(tasks.routes): creando los endpoints de un crud para las tareas

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -6,4 +6,30 @@ routesTasks.get("/", (req, res) => {
 	return res.send("aqui van las tareas");
 });
 
+router.post("/", (req, res) => {
+	const body = req.body;
+	res.json({
+		message: "created user",
+		data: body,
+	});
+});
+
+router.patch("/:id", (req, res) => {
+	const { id } = req.params;
+	const body = req.body;
+	res.json({
+		message: "updated",
+		data: body,
+		id,
+	});
+});
+
+router.delete("/:id", (req, res) => {
+	const { id } = req.params;
+	res.json({
+		message: "deleted",
+		id,
+	});
+});
+
 module.exports = routesTasks;
