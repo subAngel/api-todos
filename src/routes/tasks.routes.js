8c64fa87a642994bae8135c1f3b51131a1826be6
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-06 02:23:56 -0600

    feat(routes): creando una instancia del servicio y usando sus metodos

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,34 +1,39 @@
 const { Router } = require("express");
+const TasksService = require("../services/tasks.service");
 
 const routesTasks = Router();
+const service = new TasksService();
 
 routesTasks.get("/", (req, res) => {
-	return res.send("aqui van las tareas");
+	const tasks = service.findAll();
+	return res.json(tasks);
 });
 
-router.post("/", (req, res) => {
+routesTasks.post("/", (req, res) => {
 	const body = req.body;
+	const newTask = service.create(body);
 	res.json({
 		message: "created user",
-		data: body,
+		newTask,
 	});
 });
 
-router.patch("/:id", (req, res) => {
+routesTasks.patch("/:id", (req, res) => {
 	const { id } = req.params;
 	const body = req.body;
+	const task = service.update(id, body);
 	res.json({
 		message: "updated",
-		data: body,
-		id,
+		task,
 	});
 });
 
-router.delete("/:id", (req, res) => {
+routesTasks.delete("/:id", (req, res) => {
 	const { id } = req.params;
+	const response = service.delete(id);
 	res.json({
 		message: "deleted",
-		id,
+		response,
 	});
 });
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,34 +1,45 @@
 const express = require("express");
+const UsersService = require("../services/users.service");
 
 const router = express.Router();
+const service = new UsersService();
 
 router.get("/", (req, res) => {
-	res.send("Aqui van todos los usuario");
+	const users = service.findAll();
+	res.json(users);
+});
+
+router.get("/:id", (req, res) => {
+	const { id } = req.params;
+	const user = service.findOne(id);
+	res.json(user);
 });
 
 router.post("/", (req, res) => {
 	const body = req.body;
+	const newUser = service.create(body);
 	res.json({
 		message: "created user",
-		data: body,
+		newUser,
 	});
 });
 
 router.patch("/:id", (req, res) => {
 	const { id } = req.params;
 	const body = req.body;
+	const userModified = service.update(id, body);
 	res.json({
 		message: "updated",
-		data: body,
-		id,
+		userModified,
 	});
 });
 
 router.delete("/:id", (req, res) => {
 	const { id } = req.params;
+	const response = service.delete(id);
 	res.json({
 		message: "deleted",
-		id,
+		response,
 	});
 });
 
