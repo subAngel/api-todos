97f3038ab02fd8c62cc73ec2c2771179d1b18775
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-06 01:49:09 -0600

    feat(index): analizar solicitudes en formato JSON

--- src/index.js
+++ src/index.js
@@ -7,13 +7,19 @@ const app = express();
 
 // * configuracion y middlewares
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// * Rutas
 app.get("/", (req, res) => {
 	res.send("Hola mundo");
 });
 
 routerApi(app);
 
+// * Manejadores de errores
+
+// * Iniciar el servidor
 app.listen(3000, () => {
 	debug("api is running on https://localhost:3000/");
 });
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,4 +6,12 @@ router.get("/", (req, res) => {
 	res.send("Aqui van todos los usuario");
 });
 
+router.post("/", (req, res) => {
+	const body = req.body;
+	res.json({
+		message: "created user",
+		data: body,
+	});
+});
+
 module.exports = router;
