57d8beaeba542de5aec9fbb7faf5df9e975e605c
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-07 01:04:50 -0600

    refactor(routes.index): modificando el enrutador principal con case sensitive y strict routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,10 @@ const usersRouter = require("./users.routes");
 const tasksRouter = require("./tasks.routes");
 
 function routesApi(app) {
-	const router = express.Router();
+	const router = express.Router({
+		caseSensitive: app.get("case sensitive routing"),
+		strict: app.get("strict routing"),
+	});
 	app.use("/api", router);
 	router.use("/users", usersRouter);
 	router.use("/tasks", tasksRouter);
