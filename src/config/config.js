caf75e38b7bfd959f4630ebe2482a48114db439e
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-07 02:57:36 -0600

    Creando un archivo de configuracion que usa variables de entorno

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+const config = {
+	env: process.env.NODE_ENV || "dev",
+	port: process.env.PORT || 3080,
+	dbUser: process.env.DB_USER,
+	dbPassword: process.env.DB_PASSWORD,
+	dbName: process.env.DB_NAME,
+	dbHost: process.env.DB_HOST,
+	dbPort: process.env.DB_PORT,
+};
+
+module.exports = config;
