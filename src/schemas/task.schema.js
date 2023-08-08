3ecac6335734a851495663a44eee3c4db144aea9
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-08 02:12:18 -0600

    feat(migrations): generando archivos para hacer migraciones

--- src/schemas/task.schema.js
+++ src/schemas/task.schema.js
@@ -8,7 +8,7 @@ const status = Joi.string().valid("COMPLETED", "PENDING");
 const due_date = Joi.date();
 
 const createTaskSchema = Joi.object({
-	id: id.required(),
+	// id: id.required(),
 	user_id: user_id.required(),
 	title: title.required(),
 	description: description,
