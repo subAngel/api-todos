b45ad5014864719745296bf8ec9e07d095fe078b
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-09 22:32:56 -0600

    feat(auth.route): mandando un token en el login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const passport = require("passport");
-
+const jwt = require("jsonwebtoken");
+const { config } = require("../config/config");
 const authRouter = Router();
 
 authRouter.post(
@@ -8,7 +9,14 @@ authRouter.post(
 	passport.authenticate("local", { session: false }),
 	async (req, res, next) => {
 		try {
-			res.json(req.user);
+			const user = req.user;
+			const payload = {
+				sub: user.id,
+				user: user,
+			};
+
+			const token = jwt.sign(payload, config.secretKey);
+			res.json({ user, token });
 		} catch (error) {
 			next(error);
 		}
