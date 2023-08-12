f2f6e8a53fbbc256d4a747e3b12535d99990add5
Angel Zorrilla <whoangel.agl@gmail.com>
2023-08-11 23:36:49 -0600

    feat(auth): creando un metodo en el servicio de auth para firmar un token

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,10 @@ const { Router } = require("express");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const { config } = require("../config/config");
+const AuthService = require("../services/auth.service");
 const authRouter = Router();
+
+const service = new AuthService();
 
 authRouter.post(
 	"/login",
@@ -10,14 +13,9 @@ authRouter.post(
 	async (req, res, next) => {
 		try {
 			const user = req.user;
-			const payload = {
-				sub: user.id,
-				name: user.fullname,
-			};
-
-			const token = jwt.sign(payload, config.secretKey);
-			res.cookie("token", token, { httpOnly: true });
-			res.json({ user, token });
+			const data = service.signToken(user);
+			res.cookie("token", data.token, { httpOnly: true });
+			res.json(data);
 		} catch (error) {
 			next(error);
 		}
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,9 @@
 const UsersService = require("./users.service");
 const boom = require("@hapi/boom");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { config } = require("../config/config");
+const nodemailer = require("nodemailer");
 
 const service = new UsersService();
 class AuthService {
@@ -16,6 +19,17 @@ class AuthService {
 		delete user.dataValues.password;
 		return user;
 	}
+	signToken(user) {
+		const payload = {
+			sub: user.id,
+			name: user.fullname,
+			email: user.email,
+		};
+		const token = jwt.sign(payload, config.secretKey);
+		return { user, token };
+	}
+
+	sendMail(email) {}
 }
 
 module.exports = AuthService;
